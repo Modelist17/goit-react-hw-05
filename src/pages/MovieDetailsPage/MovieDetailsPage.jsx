188126af90ff0fe3a8559fefdae129b9fe76da28
Modelist17 <hunteruboat@gmail.com>
2024-04-30 20:33:42 +0200

    and now?

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -30,10 +30,9 @@ const MovieDetailsPage = () => {
   }, [movieId]);
 
   const handleGoBack = () => {
-    history.goBack(); 
-  };  
-//   "Функція handleGoBack не повинна повертати компонент <Navigate>; натомість, вона повинна використовувати метод історії, як-от history.goBack() або navigate(-1), щоб повернутися до попереднього розташування"
-
+    history.goBack();
+  };
+  //   "Функція handleGoBack не повинна повертати компонент <Navigate>; натомість, вона повинна використовувати метод історії, як-от history.goBack() або navigate(-1), щоб повернутися до попереднього розташування"
 
   return (
     movieDetails && (
